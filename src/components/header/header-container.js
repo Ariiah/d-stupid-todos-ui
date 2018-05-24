e5fbf7194311105a87c6a3c5110af78f14a2dac9
Kelly L <lock2846@gmail.com>
2018-05-24 15:21:39 -0600

    auth and header style rendering

--- src/components/header/header-container.js
+++ src/components/header/header-container.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './header-container.css'
 
-const Header = () => (
-  <h1>Header</h1>
-)
+// const Header = () => (
+//   <h1>Header</h1>
+// )
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header-container">
+        <a href="#title" className="title">what the todo</a>
+      </div>
+    )
+  }
+}
 
 export default Header
--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
-
 import { BrowserRouter, Switch } from 'react-router-dom'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reducers from './reducers/root-reducer'
 
-// knows it's .js file
+// knows it's a .js file
 import Header from './components/header/header-container'
 
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+
 ReactDOM.render((
-  <BrowserRouter>
-    <div>
-      <Header />
-    </div>
-  </BrowserRouter>
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Header />
+      </div>
+    </BrowserRouter>
+  </Provider>
 ), document.getElementById('root'))
 registerServiceWorker()
